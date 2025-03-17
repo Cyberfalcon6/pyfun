34bd7ca73e6ba48ffe34a45460b5830d4c8cdd75
CyberFalcon <cyberfalcon660066@gmail.com>
2025-03-17 18:31:57 +0200

    version 3

--- pyfun/pyfun.py
+++ pyfun/pyfun.py
@@ -3,7 +3,15 @@
 # txt = input()
 # print(len(txt))
 
-"""doing it in one line"""
-print("please give us your text: ", end="")
-txt = input()
-print(len(txt.replace(" ", "")))
+# """doing it in one line"""
+# print("please give us your text: ", end="")
+# txt = input()
+# print(len(txt.replace(" ", "")))
+
+
+word = input(f"Enter five phrases separated by |: ")
+
+word = word.split("|")
+for x in word:
+    print(x,":",len(x.replace(" ", "")))
+
